5080d97c35d8e4f52e8ca432882ee0036ca34d7b
isso0424 <isso04240129@gmail.com>
2020-10-13 19:41:49 +0900

    [Add]fix command chain

--- switch_url.sh
+++ switch_url.sh
@@ -2,7 +2,7 @@ trush=(`git remote -v | grep origin | grep -o "http"`)
 
 if [ $? = 1 ]; then
   user_name=(`git remote -v | grep origin | grep -o "[^{:/}]*" | sed -n 2p`)
-  repo_name=(`git remote -v | grep origin | grep -o "[^/]*\s" | sed -n 4p | grep -o "[^\s]*"`)
+  repo_name=(`git remote -v | grep origin | grep -o "[^/]*\s" | sed -n 4p`)
   git remote set-url origin https://github.com/$user_name/$repo_name
   echo "set https url"
 else
