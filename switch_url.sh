80edfd08a58be469a7704569059b3d271ef0272d
isso0424 <isso04240129@gmail.com>
2020-10-13 19:28:30 +0900

    [Add]URL切り替えコマンド書いた

---
+++ switch_url.sh
@@ -0,0 +1,13 @@
+trush=(`git remote -v | grep origin | grep -o "http"`)
+
+if [ $? = 1 ]; then
+  user_name=(`git remote -v | grep origin | grep -o "[^{:/}]*" | sed -n 2p`)
+  repo_name=(`git remote -v | grep origin | grep -o "[^/]*\s" | sed -n 4p | grep -o "[^\s]*"`)
+  git remote set-url origin https://github.com/$user_name/$repo_name
+  echo "set https url"
+else
+  user_name=(`git remote -v | grep origin | grep -o "[^/]*" | sed -n 3p`)
+  repo_name=(`git remote -v | grep origin | grep -o "[^/]*" | sed -n 4p`)
+  git remote set-url origin git@github.com:$user_name/$repo_name
+  echo "set ssh url"
+fi
