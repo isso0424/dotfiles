46e9b77db595e91717ad9aacef1899232719a5eb
isso0424 <isso04240129@gmail.com>
2020-10-13 17:15:52 +0900

    [Add]echo proxy set message

--- github_proxy.sh
+++ github_proxy.sh
@@ -1,6 +1,8 @@
 output=$(git config --global http.proxy)
 if [ $? -ne 0 ]; then
   git config --global http.proxy http://po.cc.ibaraki-ct.ac.jp:3128
+  echo "set kosen proxy"
 elif [ "http://po.cc.ibaraki-ct.ac.jp:3128" = $output ]; then
   git config --global --unset http.proxy
+  echo "unset kosen proxy"
 fi
