c1362ce301ca3ecbd57eb8bc2e0410d9d1541279
isso0424 <isso04240129@gmail.com>
2021-03-14 23:45:13 +0900

    feat: update setup

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ if [ -d ~/.config/nvim ]; then
   mkdir -p ~/.config/nvim
 fi
 
-ln -f ~/dotfiles/zshrc/.zshrc ~/.zshrc
-ln -f ~/dotfiles/zshrc/.zsh_profile ~/.zsh_profile
-ln -f ~/dotfiles/vimrc/init.vim ~/.config/nvim/init.vim
-ln -f ~/dotfiles/vimrc/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -f ~/dotfiles/tmuxconf/.tmux.conf ~/.tmux.conf
-ln -f ~/dotfiles/.xprofile ~/.xprofile
+ln -sf ~/dotfiles/zshrc/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshrc/.zsh_profile ~/.zsh_profile
+ln -sf ~/dotfiles/vimrc/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/tmuxconf/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.xprofile ~/.xprofile
+ln -sf ~/dotfiles/tigrc/.tigrc ~/.tigrc
 
 sudo chmod 777 ~/dotfiles/proxy/* ~/dotfiles/commands/*
 
