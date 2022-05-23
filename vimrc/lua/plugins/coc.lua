fd7b342ca18e9044748ed32d4790990a7de71806
isso0424 <isso04240129@gmail.com>
2022-05-23 23:14:38 +0900

    feat: coc config by lua file

---
+++ vimrc/lua/plugins/coc.lua
@@ -0,0 +1,24 @@
+vim.keymap.set(
+  'i',
+  '<TAB>',
+  function()
+    return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
+  end,
+  { expr = true, noremap = true }
+)
+
+vim.api.nvim_set_keymap(
+  'i',
+  '<CR>',
+  function()
+    return vim.fn.pumvisible() == 1 and "<C-y>" or "<CR>"
+  end,
+  { noremap = true, expr = true }
+)
+
+vim.api.nvim_set_keymap('n', 'g[', '<Plug>(coc-diagnostic-prev)', { silent = true })
+vim.api.nvim_set_keymap('n', 'g]', '<Plug>(coc-diagnostic-next)', { silent = true })
+vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { silent = true })
+vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', { silent = true })
+vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
+vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', { silent = true })
