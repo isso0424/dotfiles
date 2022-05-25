eff89a415e59ccfb97003e43cc54ff9e5fa1b7d8
isso0424 <isso04240129@gmail.com>
2022-05-25 12:13:51 +0900

    feat: configurate vim-gitgutter by lua

---
+++ vimrc/lua/plugins/vim-gitgutter.lua
@@ -0,0 +1,7 @@
+vim.opt.signcolumn = 'yes'
+vim.g.gitgutter_async = true
+vim.g.gitgutter_signmodified = 'rw'
+vim.cmd('highlight GitGutterAdd ctermfg=green')
+vim.cmd('highlight GitGutterChange ctermfg=yellow')
+vim.cmd('highlight GitGutterDelete ctermfg=red')
+vim.cmd('highlight GitGutterChangeDelete ctermfg=yellow')
