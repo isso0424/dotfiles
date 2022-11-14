96669215d7f8a4ad9732d3f348d491d555965369
isso0424 <isso04240129@gmail.com>
2022-11-15 06:22:59 +0900

    fix: delete unused setting in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,8 @@ ln -sf ~/dotfiles/zshrc/.zsh_profile ~/.zsh_profile
 ln -sf ~/dotfiles/vimrc/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/tmuxconf/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.xprofile ~/.xprofile
-ln -sf ~/dotfiles/tigrc/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/vimrc/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/vimrc/lua ~/.config/nvim/lua
 
-sudo chmod 777 ~/dotfiles/proxy/* ~/dotfiles/commands/*
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
