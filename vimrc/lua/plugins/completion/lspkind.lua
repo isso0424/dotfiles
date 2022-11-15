ad9f739fbb2e528526f5b44e27b89c7f7001ff5f
isso0424 <isso04240129@gmail.com>
2022-11-16 01:07:53 +0900

    feat: install vim-lsp

---
+++ vimrc/lua/plugins/completion/lspkind.lua
@@ -0,0 +1,10 @@
+local lspkind = require('lspkind')
+require('cmp').setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+    })
+  }
+}
