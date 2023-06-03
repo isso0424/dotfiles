bca68e3db7f1f56fe933f49ecbc2f8547082fe79
isso0424 <isso04240129@gmail.com>
2023-06-03 21:49:00 +0900

    feat: build powerline setting

---
+++ tmuxconf/generate_setting.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+file_name = "~/dotfiles/tmuxconf/.load_powerline.sh"
+if [ -e $file_name ]; then
+  rm $file_name
+fi
+
+hoge=(${$(python3 -V)//\./ })
+fuga=(${$(echo $hoge[2])// /})
+
+PYTHON_MAJOR_VERSION=$fuga[2]
+echo "source '/usr/lib/python3.$PYTHON_MAJOR_VERSION/site-packages/powerline/bindings/tmux/powerline.conf'" > $file_name
